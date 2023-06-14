e637126dbf8224e303d896ff2c35bf7f5a70cab6
scriptax <moradi.online@outlook.com>
2023-06-14 21:37:09 +0330

    seperated icons and cleaned up

--- src/components/header/DarkModeBTN.tsx
+++ src/components/header/DarkModeBTN.tsx
@@ -2,37 +2,37 @@ import { ReactElement } from "react";
 import { motion } from "framer-motion";
 import icons from "../../misc/SVGs";
 
+type ModeIconProps = {
+  iconName: string,
+}
+const ModeIcon = ({iconName}: ModeIconProps): ReactElement => {
+  let iconIndex = iconName as keyof object;
+  return (
+    <motion.div
+      className="absolute left-1/2 top-1/2"
+      initial={{ x: "-10vw", translate: "-50% -50%" }}
+      animate={{ x: "0" }}
+    >
+      {icons[iconIndex]}
+    </motion.div>
+  );
+};
+
 type darkModeBTNProps = {
   darkModeHandler: () => void;
   darkMode: boolean;
 };
-const DarkModeBTN = ({
-  darkModeHandler,
-  darkMode,
-}: darkModeBTNProps): ReactElement => {
+function DarkModeBTN({
+  darkModeHandler, 
+  darkMode
+}: darkModeBTNProps): ReactElement {
   return (
     <button
       className="relative ml-3 h-10 w-10 overflow-hidden border border-solid border-green-300 bg-green-50 outline-none hover:bg-green-300 dark:bg-transparent dark:text-green-300 dark:hover:bg-green-300 dark:hover:text-slate-900"
       onClick={darkModeHandler}
     >
-      {darkMode && (
-        <motion.div
-          className="absolute left-1/2 top-1/2"
-          initial={{ x: "-10vw", translate: "-50% -50%" }}
-          animate={{ x: "0" }}
-        >
-          {icons?.Sun}
-        </motion.div>
-      )}
-      {!darkMode && (
-        <motion.div
-          className="absolute left-1/2 top-1/2"
-          initial={{ x: "-10vw", translate: "-50% -50%" }}
-          animate={{ x: "0" }}
-        >
-          {icons.Moon}
-        </motion.div>
-      )}
+      {darkMode && <ModeIcon iconName="Sun" />}
+      {!darkMode && <ModeIcon iconName="Moon" />}
     </button>
   );
 };
