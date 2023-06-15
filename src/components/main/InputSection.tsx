59abfa6d2b5a77cfa50c5ae82c4dfc1ef8a1ffdf
scriptax <moradi.online@outlook.com>
2023-06-15 18:34:55 +0330

    seperated textarea

--- src/components/main/InputSection.tsx
+++ src/components/main/InputSection.tsx
@@ -1,25 +1,52 @@
 import { ReactElement, useState, useRef, useEffect, useContext } from "react";
 import { TransContext } from "./Main";
-import RoundBTN from "../common/SquareBTN";
+import SquareBTN from "../common/SquareBTN";
 import readClipboard from "../../misc/readClipboard";
 import ToSpeech from "./ToSpeech";
 import AlertBox from "../common/AlertBox";
 import loadingGif from "../../assets/images/loading.gif";
 
-type PropsType = {
-  loading: boolean
+type TextAreaProps = {
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
 }
-function InputSection({loading}: PropsType): ReactElement {
-  const { translation, setTranslation } = useContext(TransContext);
+const TextArea = ({setMessage}: TextAreaProps): ReactElement => {
   const textareaRef = useRef<HTMLTextAreaElement>(null); // only for adjusting height
-  const [message, setMessage] = useState<string>("");
-  const changeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+  const { translation, setTranslation } = useContext(TransContext);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLTextAreaElement>): void | undefined => {
     if (translation.input.length >= 500) {
       setMessage("Text limit reached!");
       return undefined;
     }
     setTranslation((prev) => ({ ...prev, input: e.target.value }));
   };
+  const adjustHeight = (): void => {
+    const textarea = textareaRef.current!;
+    textarea.style.height = "120px";
+    textarea.style.height = textarea.scrollHeight + "px";
+  };
+  useEffect(() => {
+    adjustHeight();
+  }, [translation.input]);
+
+  return (
+    <textarea
+      placeholder="Type here to translate"
+      dir="auto"
+      className="min-h-[120px] w-full resize-none bg-transparent text-xl font-extralight text-black outline-none hover:cursor-text dark:text-white"
+      onChange={changeHandler}
+      value={translation.input}
+      ref={textareaRef}
+    ></textarea>
+  );
+};
+
+type PropsType = {
+  loading: boolean
+}
+function InputSection({loading}: PropsType): ReactElement {
+  const { translation, setTranslation } = useContext(TransContext);
+  const [message, setMessage] = useState<string>("");
 
   const pasteHandler = async (): Promise<void> => {
     let text: string = await readClipboard();
@@ -32,7 +59,7 @@ function InputSection({loading}: PropsType): ReactElement {
 
   useEffect(() => {
     let timer: ReturnType<typeof setTimeout>;
-    if (message) {
+    if(message) {
       timer = setTimeout(() => {
         setMessage("");
       }, 2000);
@@ -42,32 +69,15 @@ function InputSection({loading}: PropsType): ReactElement {
     };
   }, [message]);
 
-  const adjustHeight = (): void => {
-    const textarea = textareaRef.current!;
-    textarea.style.height = "120px";
-    textarea.style.height = textarea.scrollHeight + "px";
-  };
-
-  useEffect(() => {
-    adjustHeight();
-  }, [translation.input]);
-
   return (
     // we can also make the section flex and direction column to send the buttons to bottom
     <section className="-mb-1 min-h-[180px] w-full max-w-xl p-3 dark:shadow-none">
       <div className="flex justify-between">
         <div className="relative w-11/12 pt-2">
-          <textarea
-            placeholder="Type here to translate"
-            dir="auto"
-            className="min-h-[120px] w-full resize-none bg-transparent text-xl font-extralight text-black outline-none hover:cursor-text dark:text-white"
-            onChange={changeHandler}
-            value={translation.input}
-            ref={textareaRef}
-          ></textarea>
+          <TextArea setMessage={setMessage} />
         </div>
         {translation.input.length > 0 && (
-          <RoundBTN
+          <SquareBTN
             iconName="Close"
             description="Clear"
             handler={() => {
@@ -78,7 +88,7 @@ function InputSection({loading}: PropsType): ReactElement {
       </div>
       <div className="relative top-1 flex w-full items-center justify-between">
         <div>
-          <RoundBTN
+          <SquareBTN
             iconName="Paste"
             description="Paste from clipboard"
             handler={() => {
