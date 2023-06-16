412cc89d3d899f659fb301390d0e4c73956fc296
scriptax <moradi.online@outlook.com>
2023-06-16 13:58:28 +0330

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,13 +8,13 @@ function Footer(): ReactElement {
   }, []);
   return (
     <footer className="h-20 m-auto w-full max-w-xl px-4 dark:text-white sm:px-0">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between text-sm">
         <span>© {date?.getFullYear()} By scriptax.</span>
         <a href="https://github.com/scriptax/translate-app" target="blank">
           {icons.Github}
         </a>
       </div>
-      <div className="text-sm pt-1">
+      <div className="text-xs pt-1">
         Translation API by <a className="underline" href="https://mymemory.translated.net/">MyMemory</a><br />
         Speech synthesis API by <a className="underline" href="https://voicerss.org/">Voice RSS</a>
       </div>
