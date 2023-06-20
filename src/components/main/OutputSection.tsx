3d802c0ecb20ba35826de95e011515f61cb40088
scriptax <moradi.online@outlook.com>
2023-06-20 15:47:17 +0330

    output section buttons position fix

--- src/components/main/OutputSection.tsx
+++ src/components/main/OutputSection.tsx
@@ -13,8 +13,8 @@ function OutputSection(): ReactElement {
   };
   useAlertTimer(copyMessage, setCopyMessage);
   return (
-    <section className="min-h- min-h-[200px] w-full max-w-xl border-t-4 border-dashed border-slate-600 bg-neutral-100 p-3 dark:border-slate-200 dark:bg-slate-800 dark:shadow-none">
-      <div className="flex justify-between">
+    <section className="flex flex-col min-h-[200px] w-full max-w-xl border-t-4 border-dashed border-slate-600 bg-neutral-100 p-3 dark:border-slate-200 dark:bg-slate-800 dark:shadow-none">
+      <div className="flex-grow flex justify-between">
         <div
           dir="auto"
           className="min-h-[120px] w-full resize-none break-words bg-transparent text-lg text-black outline-none dark:text-white "
