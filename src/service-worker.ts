3da9601a44b30fb3c74d542c890e099774c732be
scriptax <moradi.online@outlook.com>
2023-07-23 00:01:57 +0330

    initial service workers for caching assets

---
+++ src/service-worker.ts
@@ -0,0 +1,25 @@
+/// <reference lib="webworker" />
+/* eslint-disable no-restricted-globals */
+
+import { clientsClaim } from 'workbox-core';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+declare const self: ServiceWorkerGlobalScope;
+
+clientsClaim();
+
+// Precache all assets
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  new StaleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new ExpirationPlugin({ maxEntries: 50 }),
+    ],
+  })
+);
